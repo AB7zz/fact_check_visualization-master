d0b3e24a3da04c7201896ea91ba1fd26c4e1ba21
AB7zz <48093418+AB7zz@users.noreply.github.com>
2023-08-20 17:53:30 -0400

    Update google_search.py

--- google_search.py
+++ google_search.py
@@ -57,13 +57,7 @@ def parseAgain(url, article):
         return False
     
 
-
-
-# called from do_research(param)
-# search_claim(param,1 claim's text)
-# makes google search about that claim and returns an array of all articles in order what appeared on google first
 def search_claim(param, claim):
-    print("Searching with Google ...")
     urls = []
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5)\AppleWebKit/537.36 (KHTML, like Gecko) Cafari/537.36'}
@@ -74,18 +68,14 @@ def search_claim(param, claim):
 
     count = 0
     for link in soup.find_all("div", {"class": "b_title"}):
-        if count == 10:
+        if count == 7:
             break
-
         if link.find('a'):
             urls.append(link.find('a')['href'])
             count += 1
-    # urls =[articlelink,articlelink,articlelink] from google
-
     articles = []
 
     # urls = ['https://www.ajc.com/news/national/immigration-can-undocumented-immigrants-get-federal-public-benefits/nyks4aB0PtTbbwVP9GyogI/', 'https://www.politifact.com/factchecks/2019/jan/28/donald-trump/fact-checking-donald-trumps-claim-cost-illegal-imm/']
-    print(urls)
     if(len(urls) == 0):
         return search_claim(param, claim)
     else:
@@ -105,15 +95,9 @@ def search_claim(param, claim):
                     printGreen("Successfully parsed the article again :" + url)
                 else:
                     printRed("Unable to parse the article again :" + url)
-    # articles = [articledata,articledata,articledata]
     return articles
 
 
-
-# called by do_research to fill up Analyzed_Article.most_relevant_sent[sentences from articles in order of similarity]
-# we put those sentences in order using the find_most_similar in bert_embedding
-
-# analyze_article(articles text, claims text, 5)
 def analyze_article(article, claim, n_relevant):
     print('Analyzing article ...')
     relevant_sentences = find_most_similar(article, claim)
@@ -122,16 +106,7 @@ def analyze_article(article, claim, n_relevant):
         return relevant_sentences[0: n_relevant - 1]
     else:
         return None
-    # image = article.top_image
-    # nlp = article.nlp()
-    # keywords = article.keywords
-    # summary = article.summary
-    # authors = article.authors
-    # date = article.publish_date
 
-
-# called from do_research(article.text)
-# just put . in the end of article etc
 def preprocess_article_text(text):
     text = text.replace('\n', '. ')
     text = text.replace('\t', '')
@@ -139,41 +114,28 @@ def preprocess_article_text(text):
 
 
 def do_research(param, userClaim):
-    # claims = read_claims(param['input'])
     readClaim = read_claim(userClaim)
     print("Research started ...")
     c = 1
-
     printLightPurple(
         " C L A I M     # " + str(c) + " ========================================================================")
-    # max 2 articles at a time
-    # search_claim(6 links,claims[claim.text])
-    # in order of what article came on top of the google search
     relevant_articles = search_claim(param, readClaim.text)
-    # relevant articles = [articledata,articledata,articledata]
+    print("original articles =" + relevant_articles)
     analyzed_articles = []
     i = 1
-    # a = single article data
-
-    # FROM HERE WE FILL THE ARTICLE DATASTRUCTURE FOR EACH ARTICLE FOR THAT CLAIM
 
     for a in relevant_articles:
         # articledata.text because of newspaper package
         if a.text != "":
             # links = analyze_urls(a)
-            print("Processing article #" + str(
-                i))  # NOTE: the article may or may not be added to the output file based on its length")
+            print("Processing article #" + str(i))  # NOTE: the article may or may not be added to the output file based on its length")
             # just creates a article data structure and fills up artcle.text and article.id
 
             article = Analyzed_article(a.text)
             # article.preprocessed_text just the articles text with . and no tabs
             article.preprocessed_text = preprocess_article_text(a.text)
 
-            article.most_relevant_sent = analyze_article(article.preprocessed_text, readClaim.text,
-                                                            param['n_relevant_sent'])
-            # article.most_relevant_sent = [relevant sentence in order of similarity] we only fill up rest of the
-            # article data structure for that article only if there are similar sentences in that article
-            # compared to the claim
+            article.most_relevant_sent = analyze_article(article.preprocessed_text, readClaim.text,param['n_relevant_sent'])
             if article.most_relevant_sent is not None:
                 if len(a.authors) > 0:
                     article.author = a.authors
@@ -198,22 +160,9 @@ def do_research(param, userClaim):
                 i += 1
     c += 1
 
-    # claims = [(1,text,claimer, 23-23-3233, 2020),(2,text,claimer, 23-23-3233, 2020)] claim.articles = [[
-    # article,article,article,article]] filling up the Claim.articles[] for each claim to have all the articles
-    # that are analyzed claims.articles = [relevantartcledatastructure, relevantartcledatastructure,
-    # relevantartcledatastructure] in the order of which article came on google search first
-    # write_test_file(param,claim)
-
     readClaim.articles = analyzed_articles
-    # calling analyze article with 2 original articles
     for a in readClaim.articles:
         analyze_urls(a, readClaim, 1)
-
-    # called by do_research
-    # writes json data for a single claim's articles
-    # this is called in a for loop iterating over each claim
-
-    # same thing as dump_data but in a more organized way for D3
     return write_json_visualization(param, readClaim)
 
 
