96124c58f39a92b8c9b3d32f6f655c823dfeaee4
AB7zz <48093418+AB7zz@users.noreply.github.com>
2024-01-11 14:07:51 -0500

    Update analyze_hyperlinks.py

--- analyze_hyperlinks.py
+++ analyze_hyperlinks.py
@@ -1,6 +1,7 @@
 import logging
 import optparse
 from collections import OrderedDict
+import urllib.parse
 from urllib.request import Request, urlopen
 from time import sleep
 from AdvancedHTMLParser import AdvancedHTMLParser
@@ -10,60 +11,46 @@ from bert_embeddings import *
 from data_structures import Analyzed_article
 from settings import global_counter1
 from print_colors import *
-from google_search import parseAgain
+from google_search import *
 
-
-MAX_RETRIES = 2
-
-
-def parse_parameters(opts):
-    param = OrderedDict()
-    # param = {'input': inputfilename, 'output': outputfilename, 'stop','n_relevant_sent','top_search_results','json_visualization'}
-    param['input'] = opts.input
-    param['output'] = opts.output
-    param['stop'] = opts.stop
-    param['n_relevant_sent'] = opts.n_relevant_sent
-    param['top_search_results'] = opts.top_search_results
-    logging.info("PARAMETER LIST:_______________________________________________________________________")
-    logging.info(param)
-
-    return param
-
-
-def extract_urls_from_html(article):
+def extract_articles_from_html(article):
     try:
         parser = AdvancedHTMLParser()
         parser.parseStr(article.html)
         temp_links = parser.getElementsByTagName('a')
         whole_article_text = article.text
         p_article_text = parser.getElementsByTagName('p')
-
         req = Request(
             url=article.url,
             headers={'User-Agent': 'Mozilla/5.0'}
         )
+        
         webpage = urlopen(req).read()
         soup = BeautifulSoup(webpage, 'html.parser')
-        all_p_urls = [tag['href'] for tag in soup.select('p a[href]')]
-
-        # Filter out invalid URLs
-        valid_urls = []
-        for url in all_p_urls:
-            if url.startswith('http://') or url.startswith('https://') and not url.lower().endswith('.pdf'):
-                valid_urls.append(url)
-
-        # Return the first two valid URLs
-        return valid_urls[:2]
+        citation_urls_webpage = [tag['href'] for tag in soup.select('p a[href]')]
+        
+        try:
+            # Filter out invalid citation articles
+            valid_citation_articles = []
+            for citation_url in citation_urls_webpage:
+                citation_article = Article(citation_url)
+                citation_article.download()
+                citation_article.parse()
+                if citation_article.text != None:
+                    valid_citation_articles.append(citation_article)
+        except:
+            print(f"Error: Could not parse citation article")
+            
+        return valid_citation_articles, len(valid_citation_articles)
 
     except Exception as e:
         # Handle parsing or network errors here
-        print(f"Error: {e}")
+        print(f"Error: Could not parse citation  #", str())
         return []
 
 def preprocess_article_text(text):
-    text = text.replace('\n', '. ')
-    text = text.replace('\t', '')
-    return text
+    processed_text = urllib.parse.quote_plus(text)
+    return processed_text
 
 
 def analyze_article(article, claim, n_relevant):
@@ -76,83 +63,59 @@ def analyze_article(article, claim, n_relevant):
         return None
 
 
-def analyze_urls(originalarticle, claim, depth):
-    if depth < 3:
-        all_urls = extract_urls_from_html(originalarticle)
-        print("*****")
-        print("2 URLs from originalarticle ")
-        print(all_urls)
-        print("*****")
-        optparser = optparse.OptionParser()
+def get_citation_articles(originalarticle, original_article_idx, total_original, claim, depth):
+    print("Processing citation articles from originalarticle #" + str(original_article_idx) + "/" + str(total_original))
+    # while depth < 3:
+    valid_citation_articles, num_citation_articles = extract_articles_from_html(originalarticle)
+    print("# Citation articles: " + str(num_citation_articles))
+        
+        
 
-        optparser.add_option(
-            "-i", "--input", default="../input_data/enhanced_claims.txt",
-            help="path to the file containing claims"
-        )
-        optparser.add_option(
-            "-o", "--output", default="../output_data/claim",
-            help="path_to the output file, each file has a claim and its analysis"
-        )
-        optparser.add_option(
-            "-s", "--stop", default=10,
-            help="number of claims to analyze"
-        )
-        optparser.add_option(
-            "-r", "--n_relevant_sent", default=5,
-            help="n most releveant sentences in each article."
-        )
-        optparser.add_option(
-            "-t", "--top_search_results", default=15,
-            help="limit research to the first r search results retrieved."
-        )
+        # for url in all_urls:
+        #     retries = 0
+        #     while retries < MAX_RETRIES:
+        #         config = Config()
+        #         config.request_timeout = 5  # Increase the timeout to 20 seconds
 
-        opts = optparser.parse_args()[0]
-        param = parse_parameters(opts)
-        for url in all_urls:
-            retries = 0
-            while retries < MAX_RETRIES:
-                config = Config()
-                config.request_timeout = 5  # Increase the timeout to 20 seconds
-
-                article = Article(url, config=config)
-                try:
-                    article.download()
-                    article.parse()
-                    if article.text != "":
-                        analyzed_article = Analyzed_article(article.text)
-                        analyzed_article.preprocessed_text = preprocess_article_text(article.text)
-                        analyzed_article.most_relevant_sent = analyze_article(analyzed_article.preprocessed_text, claim.text, param['n_relevant_sent'])
-                        if analyzed_article.most_relevant_sent is not None:
-                            next(global_counter1)
-                            if len(article.authors) > 0:
-                                article.author = article.authors or ""
-                            if article.publish_date is not None:
-                                formatted_date = article.publish_date.strftime("%d-%b-%Y")
-                                analyzed_article.publish_date = formatted_date
-                                analyzed_article.year = article.publish_date.year
-                            if article.top_image != '':
-                                analyzed_article.image = article.top_image
-                            if article.summary != '':
-                                analyzed_article.summary = article.summary
-                            if len(article.keywords) > 0:
-                                analyzed_article.keywords = article.keywords
-                            if article.source_url != '':
-                                analyzed_article.source_url = article.source_url
-                            if article.url != '':
-                                analyzed_article.url = article.url
-                            if article.html != '':
-                                analyzed_article.html = article.html
-                            analyzed_article.depth = depth
-                        if analyzed_article.url != "UNKNOWN":
-                            originalarticle.articleurls.append(analyzed_article)
-                            analyze_urls(analyzed_article, claim, depth + 1)
-                        break  # Exit the loop on success
-                except ArticleException as ae:
-                    printRed("Error parsing the article: " + str(ae))
-                    break
-                    retries += 1
-                    sleep(5)  # Wait for 5 seconds before retrying
-                except Exception as e:
-                    printRed("Unexpected error: " + str(e))
-                    retries += 1
-                    sleep(5)
+        #         article = Article(url, config=config)
+        #         try:
+        #             article.download()
+        #             article.parse()
+        #             if article.text != "":
+        #                 analyzed_article = Analyzed_article(article.text)
+        #                 analyzed_article.preprocessed_text = preprocess_article_text(article.text)
+        #                 analyzed_article.most_relevant_sent = analyze_article(analyzed_article.preprocessed_text, claim.text, 2)
+        #                 if analyzed_article.most_relevant_sent is not None:
+        #                     next(global_counter1)
+        #                     if len(article.authors) > 0:
+        #                         article.author = article.authors or ""
+        #                     if article.publish_date is not None:
+        #                         formatted_date = article.publish_date.strftime("%d-%b-%Y")
+        #                         analyzed_article.publish_date = formatted_date
+        #                         analyzed_article.year = article.publish_date.year
+        #                     if article.top_image != '':
+        #                         analyzed_article.image = article.top_image
+        #                     if article.summary != '':
+        #                         analyzed_article.summary = article.summary
+        #                     if len(article.keywords) > 0:
+        #                         analyzed_article.keywords = article.keywords
+        #                     if article.source_url != '':
+        #                         analyzed_article.source_url = article.source_url
+        #                     if article.url != '':
+        #                         analyzed_article.url = article.url
+        #                     if article.html != '':
+        #                         analyzed_article.html = article.html
+        #                     analyzed_article.depth = depth
+        #                 if analyzed_article.url != "UNKNOWN":
+        #                     originalarticle.articleurls.append(analyzed_article)
+        #                     analyze_urls(analyzed_article, claim, depth + 1)
+        #                 break  # Exit the loop on success
+        #         except ArticleException as ae:
+        #             printRed("Error parsing the article: " + str(ae))
+        #             break
+        #             retries += 1
+        #             sleep(5)  # Wait for 5 seconds before retrying
+        #         except Exception as e:
+        #             printRed("Unexpected error: " + str(e))
+        #             retries += 1
+        #             sleep(5)
