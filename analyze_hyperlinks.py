6019fc0b5ee08d59a80a8e9705dd72620af60ab4
AB7zz <48093418+AB7zz@users.noreply.github.com>
2024-01-11 17:37:06 -0500

    Update analyze_hyperlinks.py

--- analyze_hyperlinks.py
+++ analyze_hyperlinks.py
@@ -44,7 +44,7 @@ def extract_articles_from_html(article):
                 print(f"Error: Could not parse citation article:" + str(citation_url))
                 
         print("# Citation articles successfully downloaded from article: " + str(len(valid_citation_articles)) + "\n\n")
-        return valid_citation_articles[0:3], len(valid_citation_articles)
+        return valid_citation_articles[0:1], len(valid_citation_articles)
         
     except Exception as e:
         print(f"Original article could not be parsed for links")
