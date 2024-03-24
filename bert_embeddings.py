4e84ed8c8a26a31b0b99845c272f0ace06e25851
AB7zz <48093418+AB7zz@users.noreply.github.com>
2024-03-24 18:27:59 -0400

    Update bert_embeddings.py

--- bert_embeddings.py
+++ bert_embeddings.py
@@ -103,7 +103,7 @@ def find_most_similar(article, claim):
         sentences = []
         for sent in sentence_tokens:
             if sent != '.':
-                sent = sent[:551]
+                # sent = sent[:551]
                 s = Article_sent(sent)
                 s.vector = get_sentence_vector(s.text)
                 # checks sentences in the article with the sentences in the claim
