29ee2de85afaaa016e885d6584fd4df5c498d6e7
AB7zz <48093418+AB7zz@users.noreply.github.com>
2024-03-24 19:27:44 -0400

    Update google_search.py

--- google_search.py
+++ google_search.py
@@ -67,7 +67,7 @@ def search_claim(param, claim):
         print("PHASE 1: GETTING UPTO 25 RESULTS FROM BING")
         urls = []
         headers = {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5)\AppleWebKit/537.36 (KHTML, like Gecko) Cafari/537.36''
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5)\AppleWebKit/537.36 (KHTML, like Gecko) Cafari/537.36'
         }
         
         num_results = 25
